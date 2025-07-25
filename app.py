512935c60f55fdb4873bb66a6cf0376936e6a203
Biradar Mahadevi Ganpat <119394619+BiradarMahadevi@users.noreply.github.com>
2025-07-25 18:54:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,19 +12,18 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.text("Sepal characteristics")
-    sepal_l = st.slider('Sepal length (cm)', 1.0, 8.0, 0.5)
-    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+    sepal_l = st.slider('Sepal length (cm)', 1.0, 8.0, 5.0, step=0.1)
+    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 3.0, step=0.1)
 
 with col2:
     st.text("Petal characteristics")
-    petal_l = st.slider('Petal length (cm)', 1.0, 7.0, 0.5)
-    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+    petal_l = st.slider('Petal length (cm)', 1.0, 7.0, 4.0, step=0.1)
+    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 1.2, step=0.1)
 
 st.text('')
 if st.button("Predict type of Iris"):
-    result = predict(
-        np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
-    st.text(result[0])
+    result = predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+    st.success(f'Predicted Iris class: {result[0]}')
 
     if result[0] == 'Iris-setosa':
         st.image('pic1.jpg')
@@ -35,5 +34,4 @@ if st.button("Predict type of Iris"):
 
 st.text('')
 st.text('')
-st.markdown(
-    '`CREATED BY` [PRATIBHA]')
+st.markdown('`CREATED BY` [PRATIBHA]')
